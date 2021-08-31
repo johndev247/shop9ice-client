fec939d1a43ee825b50b6489b988b1b28f3ef286
johndev247 <johndev247@gmail.com>
2021-08-31 10:07:48 +0100

    build

---
+++ build/precache-manifest.4c0b2eb7597478f23479ea6c2064b65e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "851b5e8c794f6ef283316fea6cff7e50",
+    "url": "/index.html"
+  },
+  {
+    "revision": "120ba6234c0397245a64",
+    "url": "/static/css/2.fe62fa80.chunk.css"
+  },
+  {
+    "revision": "4d5a35d526a41a41fe24",
+    "url": "/static/css/main.5a168a25.chunk.css"
+  },
+  {
+    "revision": "120ba6234c0397245a64",
+    "url": "/static/js/2.04ac41a6.chunk.js"
+  },
+  {
+    "revision": "074a984e3d6f2df40aaacb159df0c8bf",
+    "url": "/static/js/2.04ac41a6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4d5a35d526a41a41fe24",
+    "url": "/static/js/main.f526a4d2.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  }
+]);
